6412f1bd3963a5c3a9339d6ebf40570f3593a639
zahid <epiczahid@gmail.com>
2022-06-07 16:49:45 +0600

    feat: validator middleware

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { AnyZodObject, ZodError } from "zod";
+
+const validate =
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      next();
+    } catch (err: any) {
+      if (err instanceof ZodError) {
+        return res.status(400).json(err.flatten());
+      } else {
+        throw err;
+      }
+    }
+  };
+
+export default validate;
