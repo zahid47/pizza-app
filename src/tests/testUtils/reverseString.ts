f2f12136d7cc0b8ccfb0c3ca99b6323a77c5c18c
zahid <epiczahid@gmail.com>
2022-06-30 22:35:49 +0600

    fix: use const instead of let

--- src/tests/testUtils/reverseString.ts
+++ src/tests/testUtils/reverseString.ts
@@ -1,14 +1,14 @@
 export const reverseString = (str: string) => {
   // Step 1. Use the split() method to return a new array
-  let splitString = str.split(""); // let splitString = "hello".split("");
+  const splitString = str.split(""); // const splitString = "hello".split("");
   // ["h", "e", "l", "l", "o"]
 
   // Step 2. Use the reverse() method to reverse the new created array
-  let reverseArray = splitString.reverse(); // let reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  const reverseArray = splitString.reverse(); // const reverseArray = ["h", "e", "l", "l", "o"].reverse();
   // ["o", "l", "l", "e", "h"]
 
   // Step 3. Use the join() method to join all elements of the array into a string
-  let joinArray = reverseArray.join(""); // let joinArray = ["o", "l", "l", "e", "h"].join("");
+  const joinArray = reverseArray.join(""); // const joinArray = ["o", "l", "l", "e", "h"].join("");
   // "olleh"
 
   //Step 4. Return the reversed string
