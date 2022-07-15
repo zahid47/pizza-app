24282a72103b8ab67f0af2be2a025bb81d205dc6
zahid <epiczahid@gmail.com>
2022-07-15 22:48:54 +0600

    feat: register

--- client/components/Register.tsx
+++ client/components/Register.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import axios from "../utils/axios";
+import Cookies from "js-cookie";
+import { useState } from "react";
+import Router from "next/router";
 
 export default function Register() {
+  const [creds, setCreds] = useState({});
+
+  const handleRegister = (e: any) => {
+    e.preventDefault();
+
+    axios
+      .post("/user", creds)
+      .then(() => {
+        Router.push("/login");
+      })
+      .catch((err: any) => {
+        console.log(err.response.data);
+      });
+  };
+
   return (
-    <div>Register</div>
-  )
+    <div>
+      <form>
+        <label>Name: </label>
+        <input
+          type="text"
+          onChange={(e) => setCreds({ ...creds, name: e.target.value })}
+        />
+
+        <label>Email: </label>
+        <input
+          type="email"
+          onChange={(e) => setCreds({ ...creds, email: e.target.value })}
+        />
+
+        <label>Phone: </label>
+        <input
+          type="number"
+          onChange={(e) => setCreds({ ...creds, phone: e.target.value })}
+        />
+
+        <label>Address: </label>
+        <input
+          type="address"
+          onChange={(e) =>
+            setCreds({ ...creds, address: { addressLine: e.target.value } })
+          }
+        />
+
+        <label>Password: </label>
+        <input
+          type="password"
+          onChange={(e) => setCreds({ ...creds, password: e.target.value })}
+        />
+
+        <button onClick={handleRegister}>Create Account</button>
+      </form>
+    </div>
+  );
 }
