e9aac18a7f3b682c39f0040dd94e857300b3d848
zahid <epiczahid@gmail.com>
2022-07-22 22:27:10 +0600

    add a default get response

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import path from "path";
@@ -23,6 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors({ origin: process.env.clientURL }));
+
+app.get("/", (_req: Request, res: Response) => {
+  return res.sendStatus(200);
+});
 
 //Routes
 app.use("/api/v1/healthcheck", healthcheck);
