eb14b5a9ad008927196f4feef311521c03b9ffc0
zahid <epiczahid@gmail.com>
2022-07-22 21:56:41 +0600

    fix: sendEmail now works

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -6,39 +6,45 @@ import createError from "./createError";
 const constructMsg = (
   type: "VERIFY" | "RESET",
   recipientEmail: string,
-  url: string
+  token: string
 ) => {
   switch (type) {
     case "VERIFY":
+      const verifyUrl = `${process.env.HOST}:${process.env.PORT}/api/v1/user/verify/email/${token}`;
       return {
         from: `Pizza App <${process.env.ETHEREAL_EMAIL}>`,
         to: `Recipient <${recipientEmail}>`,
         subject: `Verify your pizza app email`,
-        text: `Please verify your account using this link: ${url}. It will expire in 1 day.`,
-        html: `<p><a href=${url}>Click here</a> to verify your account. The link will expire in 1 day.</p>`,
+        text: `Please verify your account using this link: ${verifyUrl}. It will expire in 1 day.`,
+        html: `<p><a href=${verifyUrl}>Click here</a> to verify your account. The link will expire in 1 day.</p>`,
       };
     case "RESET":
+      // FIXME: change host to client host
+      const resetUrl = `${process.env.HOST}:${process.env.PORT}/api/v1/auth/reset-pass/${token}`;
       return {
         from: `Pizza App <${process.env.ETHEREAL_EMAIL}>`,
         to: `Recipient <${recipientEmail}>`,
         subject: `Reset your pizza app password`,
-        text: `Please reset your password using this link: ${url}. It will expire in 1 day.`,
-        html: `<p><a href=${url}>Click here</a> to reset your password. The link will expire in 1 day.</p>`,
+        text: `Please reset your password using this link: ${resetUrl}. It will expire in 1 day.`,
+        html: `<p><a href=${resetUrl}>Click here</a> to reset your password. The link will expire in 1 day.</p>`,
       };
   }
 };
 
-export const sendEmail = async (
+export const sendEmail = (
   recipientId: string,
   recipientEmail: string,
   type: "VERIFY" | "RESET"
 ) => {
+  const email = process.env.ETHEREAL_EMAIL;
+  const password = process.env.ETHEREAL_PASSWORD;
+
   const transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
     auth: {
-      user: process.env.ETHEREAL_EMAIL,
-      pass: process.env.ETHEREAL_PASSWORD,
+      user: email,
+      pass: password,
     },
   });
 
@@ -48,16 +54,15 @@ export const sendEmail = async (
     process.env.TOKEN_SECRET,
     process.env.TOKEN_TTL
   );
-  const url = `${process.env.HOST}:${process.env.PORT}/api/v1/user/email/${token}`;
-  const message = constructMsg(type, recipientEmail, url);
+  const message = constructMsg(type, recipientEmail, token);
 
   transporter.sendMail(message, (err, info) => {
-    if (err) {
-      throw err;
-    }
+    if (err) throw err;
 
-    log.info("Message sent: %s", info.messageId);
-    // Preview only available when sending through an Ethereal account
-    log.info("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    if (process.env.NODE_ENV !== "production") {
+      log.info("Message sent: %s", info.messageId);
+      // Preview only available when sending through an Ethereal account
+      log.info("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    }
   });
 };
