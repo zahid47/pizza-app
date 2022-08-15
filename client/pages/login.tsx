c7d2cb67c174cc0ade29f4d133509d14b76223f0
zahid <epiczahid@gmail.com>
2022-08-15 23:00:22 +0600

    minor

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -18,7 +18,7 @@ export default function Login() {
     try {
       const response = await axios.post("/auth/login", creds);
       Cookies.set("accessToken", response.data.accessToken);
-      await router.replace("/");
+      await router.push("/");
       router.reload(); //FIXME: idk if this is the right way to do this
     } catch (err: any) {
       console.log(err.response.data.message);
